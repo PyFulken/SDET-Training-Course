333730be610ceb1abe6989bf213f69d833defa93
PyFulken <joao_leo1@hotmail.com>
2020-11-21 19:44:52 +0000

    Lesson 48

--- mysql_db_manipulation.py
+++ mysql_db_manipulation.py
@@ -1,6 +1,36 @@
 import mysql.connector
 from utilities.credentials import *
+from utilities.configuration import *
 
-connector = mysql.connector.connect(host="localhost", database="pythonautomation", user=get_SQL_user(), password=get_SQL_pass())
+connector = mysql.connector.connect(host=Get_Host(), database=Get_DB(), user=get_SQL_user(), password=get_SQL_pass())
 
 print(connector.is_connected())
+
+cursor = connector.cursor()
+
+cursor.execute("SELECT * FROM CustomerInfo")            
+a_single_row = cursor.fetchone()
+print(a_single_row)                                     #Returns a tuple with all the column values
+print(a_single_row[3])
+
+all_rows = cursor.fetchall()                            #Returns a LIST of tuples with each row's column values
+total_sum = 0
+for row in all_rows:
+    print(row)                                          #Note how the 1st row (Selenium) was not reprinted, cursor, once moved, stays there.
+    total_sum += row[2]
+
+print(total_sum)
+connector.close()
+
+
+#Queries with interchangeable data
+
+connector = Get_DB_Connection(get_SQL_user(), get_SQL_pass())
+
+cursor = connector.cursor()
+
+query = "UPDATE CustomerInfo SET Location = %s WHERE CourseName = %s"
+data = ("US", "Appium")                                                 #Note: Tuple, where order matters. 1st %s = tuple[0], 2nd %s = tuple[1], etc.
+
+cursor.execute(query, data)
+connector.commit()                                                      #Note: Commit is needed like git. The method is for the CONNECTOR, not the cursor.
--- utilities/configuration.py
+++ utilities/configuration.py
@@ -1,6 +1,22 @@
 import configparser
+import mysql.connector
+
+config = configparser.ConfigParser()
+config.read("SDET Training Course/utilities/config.ini")
 
 def Get_API_URL():
-    config = configparser.ConfigParser()
-    config.read("SDET Training Course/utilities/config.ini")
     return config["API"]["URL"]
+
+def Get_DB():
+    return config["SQL"]["DB"]
+
+def Get_Host():
+    return config["SQL"]["HOST"]
+
+def Get_DB_Connection(db_user, db_password):
+    try:
+        connection = mysql.connector.connect(host=Get_Host(), database=Get_DB(), user=db_user, password=db_password)
+        return connection
+    except Exception as e:
+        print(e)
+        SystemExit
